8fa2dc6bb45a9dd1e96d28456bc8934478ec8976
Hardika Joshi <90562304+hardikajoshi@users.noreply.github.com>
2023-08-02 23:50:56 +0530

    Time: 0 ms (100%), Space: 43.2 MB (89.21%) - LeetHub

--- 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.java
@@ -1,16 +1,10 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
-        int i,j;
-        for(i=0;i<arr.length;i++){
-            int count=0;
-            for(j =0;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    count++;
-                }
-            }
-            if(count> arr.length/4)
-            return arr[i];
+        int n=arr.length/4;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==arr[i+n])
+                return arr[i];
         }
-       return -1;
+        return -1;
     }
 }
