f1a34b94e51ea7ea70a45260460d7cc355891a53
Hardika Joshi <90562304+hardikajoshi@users.noreply.github.com>
2023-08-21 01:19:45 +0530

    Time: 1 ms (41.65%), Space: 41 MB (79.67%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
@@ -2,8 +2,8 @@ class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         
         int count=0;
-        for(int i=0;i<hours.length;i++){
-            if(hours[i]>=target){
+        for(int num : hours){
+            if(num>=target){
                 count++;
             }
         }
